9977aa79bc632b8a307bea19a1d45882db6c3e37
masx200 <34191203+masx200@users.noreply.github.com>
2019-06-28 13:43:06 +0800

    如果入口文件不存在则,自动生成

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,64 @@
 #!/usr/bin/env node
 "use strict";
-const { spawn } = require("child_process");
-
-// let spawnObj;
-let commandstring, command, commandargs;
-const path = require("path");
 console.log("\n");
 console.log("webpack-react-vue-spa-awesome-config");
 console.log("\n");
+console.log(
+  `极速、零配置的 web 应用打包工具, 同时支持 react 和 vue 的单页面应用,提供开箱即用支持`
+);
+
+console.log(
+  "Fast, zero-configuration web application packaging tool that supports both single-page applications for react and vue, out-of-the-box support"
+);
+console.log("\n");
+console.log(`\nworking directory : ${process.cwd()}\n`);
+console.log(`\ncommand filename : ${__filename}\n`);
+/* 如果不存在入口文件,则复制过去
+
+入口文件是"public/index.html"和'src/index.js'
+
+"public/favicon.ico"
+*/
+const path = require("path");
+const fs = require("fs");
+const pwddir = process.cwd();
+const { spawn } = require("child_process");
+
+const inputfiles = ["public/index.html", "src/index.js", "public/favicon.ico"];
+
+const sourcefiles = inputfiles.map(p =>
+  path.resolve(__dirname, "../", "release", p)
+);
+// console.log(sourcefiles);
+const destfiles = inputfiles.map(p => path.resolve(pwddir, p));
+function 判断并创建目录(p) {
+  if (!fs.existsSync(p)) {
+    console.log("所需的目录不存在,创建目录", p);
+    console.log("\n");
+    fs.mkdirSync(p);
+  }
+}
+destfiles.forEach((p, i, a) => {
+  if (!fs.existsSync(p)) {
+    console.log(`inputfile  not exsited! ${p}\n`);
+    console.log(`initialize inputfile from ${sourcefiles[i]}\n`);
+    ["public", "src"]
+      .map(t => path.resolve(pwddir, t))
+      .forEach(e => 判断并创建目录(e));
+
+    /* 判断文件夹是否存在! */
+    fs.copyFileSync(sourcefiles[i], a[i]);
+  }
+});
+//   .map(p => fs.existsSync(p));
+
+// console.log(existbool);
+
+// path.resolve();
+// fs.existsSync(path)
+// let spawnObj;
+let commandstring, command, commandargs;
+
 const webpackconfigfile = path.resolve(
   __dirname,
   "../",
@@ -35,11 +86,13 @@ if (process.argv.includes("start")) {
   //   console.log(commandstring);
   //   spawnObj = spawn(command, commandargs, { cwd: process.cwd() });
 } else {
+  console.log("\n");
   console.log("usage:");
   console.log("\n");
   console.log("webpack-react-vue-spa-awesome-config start");
   console.log("\n");
   console.log("webpack-react-vue-spa-awesome-config build");
+  console.log("\n");
   return;
 }
 // console.log(spawnObj);
