f0d5e12a99413ad5400b3a5e4cbf6915795cbf04
masx200 <34191203+masx200@users.noreply.github.com>
2019-06-30 23:34:45 +0800

    Add files via upload

--- release/config/webpack.config.js
+++ release/config/webpack.config.js
@@ -123,7 +123,7 @@ module.exports = {
               // https://github.com/facebook/create-react-app/issues/2677
               ident: "postcss",
               plugins: () => [
-                require("postcss-cssnext"),
+                // require("postcss-cssnext"),
                 require("postcss-flexbugs-fixes"),
                 require("postcss-preset-env")({
                   autoprefixer: {
@@ -193,7 +193,7 @@ module.exports = {
           },
           {
             loader: "fast-sass-loader",
-            options: {  }
+            options: {}
           }
         ]
       },
