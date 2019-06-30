e9f4701f7c521675deb08296f27ae05c1f684bb9
masx200 <34191203+masx200@users.noreply.github.com>
2019-06-30 23:46:21 +0800

    Update index.js

--- release/src/index.js
+++ release/src/index.js
@@ -1,1 +1,9 @@
-alert("hello world");
+// alert("hello world");
+import React from "react";
+import { render } from "react-dom";
+
+function 主页() {
+  return <div>hello world</div>;
+}
+
+render(<主页 />, document.getElementById("root"));
