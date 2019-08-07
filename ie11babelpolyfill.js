aa91f4eb85897ec27474affae3d49ad5c2b3dd19
masx200 <34191203+masx200@users.noreply.github.com>
2019-08-07 19:39:26 +0800

    Update ie11babelpolyfill.js

--- ie11babelpolyfill.js
+++ ie11babelpolyfill.js
@@ -5,24 +5,20 @@ var importScripts = require("./importscripts.js");
     // var s = document.createElement("script");
     // s.src = "https://cdn.bootcss.com/babel-polyfill/7.4.4/polyfill.min.js";
     // document.head.appendChild(s);
-if(typeof Promise!="function"||typeof Symbol!="function"
-||typeof Reflect!="object"
-
-){
-
-importScripts(
-      "https://cdn.bootcss.com/babel-polyfill/7.4.4/polyfill.min.js"
-    );
-}
-    
+    if (
+      typeof Promise != "function" ||
+      typeof Symbol != "function" ||
+      typeof Reflect != "object"
+    ) {
+      importScripts(
+        "https://cdn.bootcss.com/babel-polyfill/7.4.4/polyfill.min.js"
+      );
+    }
 
     // importScripts("https://cdn.staticfile.org/fetch/3.0.0/fetch.min.js");
- 
-if(typeof fetch!="function"){
-require("./fetch.js");
-}
 
- }
+    if (typeof fetch != "function") {
+      require("./fetch.js");
+    }
+  }
 })();
-
-
