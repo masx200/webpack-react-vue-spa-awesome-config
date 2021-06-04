c57d26c4116361437826ed55078e439088695d36
masx200 <34191203+masx200@users.noreply.github.com>
2021-06-04 11:04:27 +0800

    Update copy-extrance.js

---
+++ cli/src/copy-extrance.js
@@ -0,0 +1,26 @@
+import fs from "fs";
+import path from "path";
+import process from "process";
+import 判断并创建目录 from "./ensure-dir";
+const pwddir = process.cwd();
+/**
+ * @param {(import("fs").PathLike)[]} sourcefiles
+ * @param {string[]} destfiles
+ */
+export default function 生成入口文件(sourcefiles, destfiles) {
+    /**
+     * @param {import("fs").PathLike} p
+     * @param {number} i
+     *
+     */
+    destfiles.forEach((p, i) => {
+        if (!fs.existsSync(p)) {
+            console.log(`inputfile  not exsited! ${p}\n`);
+            console.log(`initialize inputfile from ${sourcefiles[i]}\n`);
+            ["public", "src"]
+                .map((t) => path.resolve(pwddir, t))
+                .forEach((e) => 判断并创建目录(e));
+            fs.copyFileSync(sourcefiles[i], p);
+        }
+    });
+}
